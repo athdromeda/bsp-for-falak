1e0880a4b44e8dac87adf00f799db1367d6cba2a
athdromeda <anixatho01@gmail.com>
2026-04-19 00:29:42 +0700

    code: add cmds on other bsp versions

---
+++ raw/de435.cmd
@@ -0,0 +1,4 @@
+LEAPSECONDS_KERNEL = naif0012.tls
+SPK_KERNEL         = de435_minified.bsp
+SOURCE_SPK_KERNEL  = de435.bsp
+   BODIES          = 10, 301, 399
---
+++ raw/de442.cmd
@@ -0,0 +1,4 @@
+LEAPSECONDS_KERNEL = naif0012.tls
+SPK_KERNEL         = de442_minified.bsp
+SOURCE_SPK_KERNEL  = de442.bsp
+   BODIES          = 10, 301, 399
